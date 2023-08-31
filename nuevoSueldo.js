2066e2c2ca32ecbdcc79d6043880f8fed49b3932
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-08-31 14:26:41 -0300

    Ejercicio 2 resuelto

---
+++ nuevoSueldo.js
@@ -0,0 +1,13 @@
+let nombre = 'Osvaldo';
+let apellido = 'Matias';
+let sueldoActual = 10000;
+const porcentajeAumento = 25;
+let calculoAumento = (sueldoActual*porcentajeAumento)/100;
+let nuevoSueldo = calculoAumento + sueldoActual;
+
+
+console.log(`Hola, estimado ${nombre} ${apellido} en base a su sueldo actual ha recibido un 
+aumento de ${porcentajeAumento}%: ${nuevoSueldo}.`)
+
+
+
