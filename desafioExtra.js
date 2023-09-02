22525bff4d9b99a7b88d6639948f0e98baafdc61
Osvaldo Matias Corneli Nassif <122895353+OsvaldoCorneli@users.noreply.github.com>
2023-09-02 14:41:22 -0300

    Termine ejercicios Clase 3 DH

---
+++ desafioExtra.js
@@ -0,0 +1,20 @@
+let numero1 = 16;
+let numero2 = 2
+
+let resultado;
+
+resultado = numero1 + numero2;
+
+console.log('Resultado suma', resultado);
+
+resultado = numero1 - numero2;
+
+console.log('Resultado resta', resultado);
+
+resultado = numero1 * numero2;
+
+console.log('resultado multiplicacion', resultado )
+
+console.log('resultado division', numero1/numero2);
+
+
